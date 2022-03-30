fa051c41bc5d5941f9e6154868234cdeb65efd44
unknown <tsanchit92@gmail.com>
2022-03-30 15:54:09 +0530

    tdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+
+  constructor(private _enrollementService:EnrollmentService){}
+
+  topics =['Angular','React','Vue'];
+  topicHasError=true;
+  submitted =false;
+  errorMsg='';
+
+
+   userModel = new User('','tsanchit92@gmail.com',1234567891,'default','morning',true);
+
+   validateTopic(value:any)
+   {
+     if(value==='default')
+     {
+       this.topicHasError=true;
+     }
+     else{
+       this.topicHasError=false;
+     }
+   }
+
+   onSubmit()
+   {
+     alert("form submitted succesfully");
+     this.submitted=true;
+     this._enrollementService.enroll(this.userModel)
+      .subscribe(
+        data=>console.log('Succes!',data),
+        error=>this.errorMsg=error.statusText
+      )
+   }
 }
